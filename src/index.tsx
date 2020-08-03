adf09be69a17fd7a800e62ce47c24180a4ec1c35
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-08-03 14:33:15 +0900

    Modified src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Root from "./Root";
+import Root from "./components/Root";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
