379d2fef0b7a97e6e216964adf359153fc2d98ff
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-08-03 14:47:18 +0900

    Feed page that will be shown to users when they log in

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Navigator from '../components/navigator';
+
+const No1: React.FC = () => (
+  <>
+    <Navigator />
+    <h1> No1 입니다.</h1>
+  </>
+);
+
+export default No1;
